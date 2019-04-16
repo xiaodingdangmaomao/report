d36d878320ec974dfc7394718216469b94f5d96a
dingdang <18818399151@163.com>
2019-04-17 00:08:38 +0800

    ceshi api8

--- test_demo.py
+++ test_demo.py
@@ -4,3 +4,15 @@ def test_demo1():
 
 def test_demo2():
     assert 2 == 3
+
+def test_demo3():
+    assert 3 == 5
+
+def test_demo4():
+    assert "ok" == "ok"
+
+def test_demo5():
+    assert 1 == 1
+
+def test_demo6():
+    assert 2 == 2
